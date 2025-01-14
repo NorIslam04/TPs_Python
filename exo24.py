4ffdb1bb92acc5ac1d15db8fa6bbb66bb42c4bc3
NorIslam04 <islamaoudi18@gmail.com>
2025-01-14 18:10:51 +0100

    update exo_24

--- exo24.py
+++ exo24.py
@@ -1,4 +1,4 @@
-def nmbr_char(str):
+def dict_char(str):
     char_counter = {}
     for char in str:
         if char in char_counter:
@@ -11,15 +11,7 @@ def nmbr_char(str):
 def anagrams(str1 :str,str2:str):
     if len(str1)!=len(str2):
         return False
-    char_counter1=nmbr_char(str1)
-    char_counter2=nmbr_char(str2)
-    for char in str1:
-        if char not in str2:
-            return False
-        # char in str1 and str2 -> no error 
-        if char_counter1[char] != char_counter2[char]:
-            return False
-    return True
+    return dict_char(str1) == dict_char(str2)
 
 
 print(anagrams("abc","der"))
